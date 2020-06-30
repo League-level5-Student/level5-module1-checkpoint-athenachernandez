cb8f757e3a36956b24cf4cc171c9203257fd0af6
Athena Hernández <64385980+athenachernandez@users.noreply.github.com>
2020-06-30 15:27:22 -0700

    y

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -5,8 +5,9 @@ public class FasterFunctions {
 		long startTime = System.currentTimeMillis();
 		//Your Code Here
 		
-		
-		
+		Thread t1 = new Thread(()->System.currentTimeMillis());
+		Thread t2 = new Thread(()->System.currentTimeMillis());
+		Thread t3 = new Thread(()->System.currentTimeMillis());
 		
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
 		totalTime += endTime;
